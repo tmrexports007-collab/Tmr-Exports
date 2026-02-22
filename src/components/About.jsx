678e9d67a47d2d1298b9b72a9f2781dddce86a2b
reema666 <reemasajreen@gmail.com>
2026-02-22 13:14:43 +0530

    before pulling shifa's work

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='font-sans'>
+       <h2 className='font-bold text-3xl '>About us</h2>
+     <div className='flex justify-center max-7xl'>
+   {/*Left side */}
+      <div>
+       <img src="" className='rounded-xl  '></img>
+      </div>
+
+      {/*Right side */}
+      <div>
+        
+      </div>
+     </div>
+
+    </div>
   )
 }
 
